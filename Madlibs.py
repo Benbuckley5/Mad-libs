02188a71b8662f09b2e353cafb9099750437524d
Benbuckley5 <82114553+Benbuckley5@users.noreply.github.com>
2021-04-10 15:22:35 +0100

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,10 @@
+def Madlib():
+    a = input('Adjective: ')
+    b = input('Noun: ')
+    c = input('Past tense verb: ')
+    d = input('Adverb: ')
+    e = input('Adjective: ')
+    f = input('Noun: ')
+    print(f'Today I went to the zoo. I saw a(n) {a} {b} jumping up and down in its tree. He {c} {d} through the large tunnel that led to its {e} {f}. ')
+
+Madlib()
